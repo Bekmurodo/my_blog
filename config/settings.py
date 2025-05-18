0350caeb1598e901d7dc14e1fe2f2b605da02978
Bekmurodo <zsuhrob240@gmail.com>
2025-05-18 15:42:09 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -88,10 +88,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'farukh_blog',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'farukh2002',
+
     }
 }
+
 
 
 # Password validation
